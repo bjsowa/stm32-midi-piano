94846aced12ae066597b07cdf6493433e8d5a543
Błażej Sowa <bsowa123@gmail.com>
2020-05-03 12:32:32 +0200

    simplify main

--- src/main.c
+++ src/main.c
@@ -21,23 +21,18 @@ static bool key_states[8];
 static uint8_t key_number[8] = { 44, 45, 46, 47, 48, 49, 50, 51 };
 static uint16_t key_gpio[8]
     = { GPIO0, GPIO1, GPIO2, GPIO3, GPIO4, GPIO5, GPIO6, GPIO7 };
-
-static void usbmidi_data_rx_cb(usbd_device *usbd_dev, uint8_t ep) {
-  char buf[64];
-  int len = usbd_ep_read_packet(usbd_dev, 0x01, buf, 64);
-}
+static uint16_t key_gpios;
 
 static void usbmidi_set_config(usbd_device *usbd_dev, uint16_t wValue) {
-  (void)wValue;
-
-  usbd_ep_setup(usbd_dev, 0x01, USB_ENDPOINT_ATTR_BULK, 64, usbmidi_data_rx_cb);
   usbd_ep_setup(usbd_dev, 0x81, USB_ENDPOINT_ATTR_BULK, 64, NULL);
-
   usb_configured = true;
 }
 
-static void setup(void) {
+static void clock_setup(void) {
   rcc_clock_setup_in_hse_8mhz_out_72mhz();
+}
+
+static void gpio_setup(void) {
   rcc_periph_clock_enable(RCC_GPIOA);
   rcc_periph_clock_enable(RCC_GPIOB);
   rcc_periph_clock_enable(RCC_AFIO);
@@ -48,22 +43,37 @@ static void setup(void) {
       GPIOB, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO10);
   gpio_set(GPIOB, GPIO10);
 
-  gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN,
-      GPIO0 | GPIO1 | GPIO2 | GPIO3 | GPIO4 | GPIO5 | GPIO6 | GPIO7);
-  gpio_clear(
-      GPIOA, GPIO0 | GPIO1 | GPIO2 | GPIO3 | GPIO4 | GPIO5 | GPIO6 | GPIO7);
+  for (int i = 0; i < 8; ++i) key_gpios |= key_gpio[i];
 
+  gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN, key_gpios);
+  gpio_clear(GPIOA, key_gpios);
+
+  // LED
+  gpio_set_mode(
+      GPIOB, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO12);
+  gpio_clear(GPIOB, GPIO12);
+}
+
+static void tim_setup(void) {
   dwt_enable_cycle_counter();
+
+  systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
+  systick_set_reload(99999);
+  systick_interrupt_enable();
+  systick_counter_enable();
+}
+
+static void usb_setup(void) {
+  desig_get_unique_id_as_string(usb_serial_number, sizeof(usb_serial_number));
+
+  usbd_dev = usbd_init(&st_usbfs_v1_usb_driver, &dev, &config, usb_strings, 3,
+      usbd_control_buffer, sizeof(usbd_control_buffer));
+
+  usbd_register_set_config_callback(usbd_dev, usbmidi_set_config);
 }
 
 void sys_tick_handler() {
-  if (!usb_configured) return;
-
-  // char buf[4] = { 0x08, 0x80, 51, 64 };
-
-  uint16_t key_presses = gpio_get(
-      GPIOA, key_gpio[0] | key_gpio[1] | key_gpio[2] | key_gpio[3] | key_gpio[4]
-                 | key_gpio[5] | key_gpio[6] | key_gpio[7]);
+  uint16_t key_presses = gpio_get(GPIOA, key_gpios);
 
   for (int i = 0; i < 8; ++i) {
     if (key_presses & key_gpio[i]) {
@@ -80,39 +90,18 @@ void sys_tick_handler() {
       }
     }
   }
-
-  // if (pressed && !key_pressed) {
-  //   key_pressed = true;
-  //   buf[0] ^= 0x01;
-  //   buf[1] ^= 0x10;
-  //   usbd_ep_write_packet(usbd_dev, 0x81, buf, sizeof(buf));
-  // } else if (!pressed && key_pressed) {
-  //   key_pressed = false;
-  //   usbd_ep_write_packet(usbd_dev, 0x81, buf, sizeof(buf));
-  // }
-
-  // uint32_t ticks = dwt_read_cycle_counter();
-  // gpio_toggle(GPIOB, GPIO12);
 }
 
+static void midi_event_poll() {}
+
 int main(void) {
-  setup();
-
-  desig_get_unique_id_as_string(usb_serial_number, sizeof(usb_serial_number));
-
-  usbd_dev = usbd_init(&st_usbfs_v1_usb_driver, &dev, &config, usb_strings, 3,
-      usbd_control_buffer, sizeof(usbd_control_buffer));
-
-  usbd_register_set_config_callback(usbd_dev, usbmidi_set_config);
-
-  usbd_poll(usbd_dev);
-
-  systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
-  systick_set_reload(99999);
-  systick_interrupt_enable();
-  systick_counter_enable();
+  clock_setup();
+  gpio_setup();
+  tim_setup();
+  usb_setup();
 
   while (1) {
     usbd_poll(usbd_dev);
+    midi_event_poll();
   }
 }
